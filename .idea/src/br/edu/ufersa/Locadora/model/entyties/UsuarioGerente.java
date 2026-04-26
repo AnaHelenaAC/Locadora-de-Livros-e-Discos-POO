cc7c88527e1b0d47bf76d0ea0698b159a4ced8a5
AnaHelenaAC <ana.cosme@alunos.ufersa.edu.br>
2026-04-25 22:15:34 -0300

    Update UsuarioGerente.java
    Atualizando o set da classe de acordo com o encapsulamento e melhoria no construtor, adição do salvamento e verificação do idGerente.

--- .idea/src/br/edu/ufersa/Locadora/model/entyties/UsuarioGerente.java
+++ .idea/src/br/edu/ufersa/Locadora/model/entyties/UsuarioGerente.java
@@ -4,8 +4,9 @@ public class UsuarioGerente extends Usuario {
     private int idGerente;
 
     public UsuarioGerente(String nome, String login, String senha) {
-        // O comando 'super' repassa esses dados lá para a classe Usuario!
-        super(nome, login, senha);
+
+        super(nome, login, senha);  // Herda os dados presentes na classe Usuario
+        setIdGerente(idGerente);    // Salva e valida o ID da classe
     }
 
     public int getIdGerente() {
@@ -13,6 +14,16 @@ public class UsuarioGerente extends Usuario {
     }
 
     public void setIdGerente(int idGerente) {
-        this.idGerente = idGerente;
+
+        // Verifica se o ID é maior que 0
+        if (idGerente <= 0){
+            System.out.println("Acesso Negado. O ID deve ser maior que 0!");
+        }
+        else {
+            this.idGerente = idGerente;
+        }
+
     }
+
+    
 }
