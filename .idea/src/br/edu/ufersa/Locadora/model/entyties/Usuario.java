af1ef5c098c86ecffd29ef3bf0441035baef10ec
AnaHelenaAC <ana.cosme@alunos.ufersa.edu.br>
2026-04-25 21:08:48 -0300

    Update em Usuario.java
    Adicionei o método fazerLogin na classe Usuario e comentei o código de toda a classe

--- .idea/src/br/edu/ufersa/Locadora/model/entyties/Usuario.java
+++ .idea/src/br/edu/ufersa/Locadora/model/entyties/Usuario.java
@@ -1,11 +1,12 @@
 package br.edu.ufersa.Locadora.model.entyties;
 
+// Classe "Mãe" Usuario
 public class Usuario {
     private String nome;
     private String login;
     private String senha;
 
-    // Construtor Usuario
+    // Construtor Usuário
     public Usuario(String nome, String login, String senha) {
         setNome(nome);
         setLogin(login);
@@ -17,7 +18,8 @@ public class Usuario {
     }
 
     public void setNome(String nome) {
-        // Se o nome for inválido, apenas avisa na tela
+
+        // Avisa na tela se o nome for inválido
         if (nome == null || nome.trim().isEmpty()) {
             System.out.println("O nome não pode ser vazio.");
         } else {
@@ -31,8 +33,12 @@ public class Usuario {
     }
 
     public void setLogin(String login) {
+
+        // Verifica se o local do login está vazio
         if (login == null || login.trim().isEmpty()) {
             System.out.println("O login não pode ser vazio.");
+
+        // Verifica se existem espaços no login
         } else if (login.contains(" ")) {
             System.out.println("O login não pode conter espaços.");
         } else {
@@ -45,6 +51,8 @@ public class Usuario {
     }
 
     public void setSenha(String senha) {
+
+        // Verifica se a senha digitada apresenta 6 caracteres
         if (senha != null && senha.length() >= 6) {
             this.senha = senha;
         } else {
@@ -52,5 +60,31 @@ public class Usuario {
         }
     }
 
+    // Método responsável por fazer o login (retorna verdadeiro ou falso)
+    public boolean fazerLogin(String loginRecebido, String senhaRecebida){
+        boolean loginCorreto;
+        boolean senhaCorreta;
+
+        // Verificando a igualdade das informações recebidas (login e senha)
+        loginCorreto = this.getLogin().equals(loginRecebido);
+        senhaCorreta = this.getSenha().equals(senhaRecebida);
+
+        // Testando se o login digitado está incoerente
+        if (loginCorreto == false){
+            System.out.println("Acesso Negado. Login incorreto!");
+            return false;
+        }
+        // Testando se a senha está incoerente
+        else if (senhaCorreta == false){
+            System.out.println("Acesso Negado. Senha incorreta!");
+            return false;
+        }
+        // Caso a senha e o login estejam corretos
+        else {
+            System.out.println("Acesso liberado.");
+            return true;
+        }
+
+    }
 
 }
