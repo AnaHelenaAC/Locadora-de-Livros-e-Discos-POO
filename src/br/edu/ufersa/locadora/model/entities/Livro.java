c040c10966d7c8c468d9ac69299ed9fdbf42695e
Gilmar Queiroz <138153124+Github4209001@users.noreply.github.com>
2026-04-27 21:29:07 -0300

    Update Livro.java
    Adicionei a lista de objetos como um dos atributos. Quando o objeto for construído, ele será adicionado a lista de livros. Adicionei também uma checagem de números negativos à quantidade de páginas, além de verificar se o número de páginas é igual a 0 e enviar um aviso.

--- src/br/edu/ufersa/locadora/model/entities/Livro.java
+++ src/br/edu/ufersa/locadora/model/entities/Livro.java
@@ -2,23 +2,29 @@ package br.edu.ufersa.locadora.model.entities;
 
 import br.edu.ufersa.Locadora.model.entities.ItemAcervo;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Livro extends ItemAcervo {
-  //Atributo:
+  //Atributos:
+  private static List<Livro> livros = new ArrayList<>(); //Lista de livros
   private int qtdPaginas = 0;
-
-  //Métodos:
-
   //getter:
   public int getQtdPaginas() {
     return qtdPaginas;
   }
   //setter:
   public void setQtdPaginas(int qtdPaginas) {
+    if (qtdPaginas < 0) throw new IllegalArgumentException("A quantidade de páginas não pode ser negativa.");
+    if (qtdPaginas == 0) System.out.println("A quantidade de páginas está registrada como 0. É possível que a quantidade de páginas não tenha sido registrada");
     this.qtdPaginas = qtdPaginas;
   }
   //construtor:
   public Livro(String titulo, String criadoPor, String genero, double valor, String dataDeLancamentoFormatada, int qtdItens, boolean isDisco, int qtdPaginas) {
     super(titulo, criadoPor, genero, valor, dataDeLancamentoFormatada, qtdItens, isDisco);
+    livros.add(this); //Adiciona o objeto criado à lista de livros
+    if (qtdPaginas < 0) throw new IllegalArgumentException("A quantidade de páginas não pode ser negativa.");
+    if (qtdPaginas == 0) System.out.println("A quantidade de páginas está registrada como 0. É possível que a quantidade de páginas não tenha sido registrada");
     this.qtdPaginas = qtdPaginas;
   }
 }
