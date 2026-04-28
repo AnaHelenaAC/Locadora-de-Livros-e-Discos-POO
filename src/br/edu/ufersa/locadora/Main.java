8f4114ea8e997e5b08666bbfd9dd3462ecd8047d
Adriam Sousa <ratogordo27@gmail.com>
2026-04-27 22:52:44 -0300

    Update Main.java
    adiciona teste para aluguel e print.

--- src/br/edu/ufersa/locadora/Main.java
+++ src/br/edu/ufersa/locadora/Main.java
@@ -1,16 +1,81 @@
 package br.edu.ufersa.locadora;
 
 import br.edu.ufersa.locadora.model.entities.Cliente;
+import br.edu.ufersa.locadora.model.entities.Aluguel;
+import br.edu.ufersa.locadora.model.entities.ItemAcervo;
+import br.edu.ufersa.locadora.model.entities.Livro;
+
+import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
 
-        //cria lista de clientes e testa os metodos.
-        Cliente c1 = new Cliente("Gadelha", "Rua Nota 7", "100.010.110-01");
+        //TESTE CLIENTE
+        Cliente c1 = new Cliente("Paulo", "Rua São Paulo", "paulo@email.com", "999999999", "12345678901");
+        Cliente c2 = new Cliente("Gadelha", "Rua Arredonda Para Baixo", "gadelha@email.com", "010203040", "45678901234");
 
         c1.cadastrar();
-        c1.alterar("Cool Gadelha", "Rua Nota 10");
+        c2.cadastrar();
+
+        System.out.println("\nLista de Clientes:");
+        for (Cliente c : Cliente.getClientes()) {
+            System.out.println(c);
+        }
+
         c1.excluir();
+
+        c2.alterar("Cool Gadelha", "Rua do Ponto Extra", "coolgadelha@email.com", "992345678");
+
+        System.out.println("\nLista de Clientes após uma exclusão e uma edição:");
+        for (Cliente c : Cliente.getClientes()) {
+            System.out.println(c);
+        }
+
+        //LIVRO
+        Livro livro = new Livro("livreto", "alguem", "branco", 10.0, "01/01/2008", 5, false, 464);
+
+        //TESTE DE ALUGUEL
+        //atrasado
+        Aluguel a1 = new Aluguel(c2, livro, LocalDate.now().minusDays(5), 3);
+        a1.registrar();
+
+        //ativo
+        Aluguel a2 = new Aluguel(c2, livro, LocalDate.now(), 3);
+        a2.registrar();
+
+        System.out.println("\nLista de Aluguéis:");
+        for (Aluguel a : Aluguel.getAlugueis()) {
+            System.out.println(a);
+        }
+
+        System.out.println("\nStatus aluguel1: " + a1.getStatus());
+        System.out.println("Status aluguel2: " + a2.getStatus());
+
+        a1.finalizarAluguel();
+
+        double valorFinal = a1.calcularValorFinal();
+        System.out.println("\nValor final do aluguel1 após finalizado com atraso: R$ " + valorFinal);
+
+        //FILTRO POR STATUS
+        System.out.println("\nAluguéis FINALIZADOS:");
+        for (Aluguel a : Aluguel.getAlugueis()) {
+            if (a.getStatus().equals("FINALIZADO")) {
+                System.out.println(a);
+            }
+        }
+
+        System.out.println("\nAluguéis ATRASADOS:");
+        for (Aluguel a : Aluguel.getAlugueis()) {
+            if (a.getStatus().equals("ATRASADO")) {
+                System.out.println(a);
+            }
+        }
+
+        System.out.println("\nAluguéis ATIVOS:");
+        for (Aluguel a : Aluguel.getAlugueis()) {
+            if (a.getStatus().equals("ATIVO")) {
+                System.out.println(a);
+            }
+        }
     }
 }
-
