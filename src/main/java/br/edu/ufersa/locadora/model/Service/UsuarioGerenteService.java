562a186e62264d28d453e7e3ae6175302d495100
AnaHelenaAC <ana.cosme@alunos.ufersa.edu.br>
2026-05-31 15:49:26 -0300

    Create UsuarioGerenteService.java

---
+++ src/main/java/br/edu/ufersa/locadora/model/Service/UsuarioGerenteService.java
@@ -0,0 +1,31 @@
+package br.edu.ufersa.locadora.model.Service;
+
+import br.edu.ufersa.locadora.model.DAO.UsuarioGerenteDAO;
+import br.edu.ufersa.locadora.model.entities.UsuarioGerente;
+import java.util.List;
+
+public class UsuarioGerenteService {
+
+    private UsuarioGerenteDAO dao = new UsuarioGerenteDAO();
+
+    // Funções referentes ao CRUD usado no DAO
+    public UsuarioGerente salvar(UsuarioGerente g) {
+        return dao.Create(g);
+    }
+
+    public UsuarioGerente buscarPorNome(String nome) {
+        List<UsuarioGerente> resultados = dao.Read(nome);
+        if (!resultados.isEmpty()) {
+            return resultados.get(0);
+        }
+        return null;
+    }
+
+    public boolean atualizar(UsuarioGerente g) {
+        return dao.Update(g);
+    }
+
+    public boolean deletar(UsuarioGerente g) {
+        return dao.Delete(g);
+    }
+}
