1a90dff56fd92513d286eb470015f8153e04c76e
Gilmar Queiroz <138153124+Github4209001@users.noreply.github.com>
2026-05-31 23:55:23 -0300

    Create LivroService.java

---
+++ src/main/java/br/edu/ufersa/locadora/model/Service/LivroService.java
@@ -0,0 +1,55 @@
+package br.edu.ufersa.locadora.model.Service;
+
+import br.edu.ufersa.locadora.model.DAO.LivroDAO;
+import br.edu.ufersa.locadora.model.entities.Livro;
+import br.edu.ufersa.locadora.exceptions.LivroException;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class LivroService {
+    private final LivroDAO livroDAO;
+    
+    public LivroService(LivroDAO LivroDAO) {
+        this.livroDAO = livroDAO;
+    }
+    
+    public void adicionarLivro(Livro livro) {
+        if (livro == null) throw new LivroException("Não é possível enviar um formulário vazio.");
+        if (livro.getTitulo().length() > 100 || livro.getCriadoPor().length() > 100 || livro.getGenero().length() > 100) throw new LivroException("Campos de texto do formulário (Título, Criado Por e Gênero) não podem conter mais de 100 caracteres.");
+        if (livro.getValor() < 0 || livro.getQtdItens() < 0 || livro.getQtdPaginas() < 0) throw new LivroException("Os campos Valor, Quantidade de Itens e Quantidade de Páginas não podem ter valores negativos.");
+        
+        livroDAO.create(livro);
+        System.out.prinln(livro.getTitulo() + " adicionado com sucesso.");
+    }
+    public list<Livro> lerLivro() {
+        if (livroDAO.read().isEmpty()) throw new LivroException("Nenhum livro foi encontrado no banco de dados.");
+        else {
+            System.out.println("Livro(s) encontrado(s).");
+            return livroDAO.read();
+        }
+    }
+    public Livro lerLivroPorID(String ID) {
+        if (livroDAO.readByID(ID) == null) throw new LivroException("Nenhum livro foi com o ID utilizado (" + ID + ") foi encontrado no banco de dados.");
+        else {
+            System.out.println("Livro encontrado.");
+            return livroDAO.readByID(ID);
+        }
+    }
+    public void atualizarLivro(Livro livro) {
+        if (livro == null) throw new LivroException("Não é possível enviar um formulário vazio.");
+        if (livroDAO.readByID(ID) == null) throw new LivroException("Nenhum livro foi com o ID utilizado (" + ID + ") foi encontrado no banco de dados.");
+        if (livro.getTitulo().length() > 100 || livro.getCriadoPor().length() > 100 || livro.getGenero().length() > 100) throw new LivroException("Campos de texto do formulário (Título, Criado Por e Gênero) não podem conter mais de 100 caracteres.");
+        if (livro.getValor() < 0 || livro.getQtdItens() < 0 || livro.getQtdPaginas() < 0) throw new LivroException("Os campos Valor, Quantidade de Itens e Quantidade de Páginas não podem ter valores negativos.");
+
+        livroDAO.update(livro);
+        System.out.prinln(livro.getTitulo() + " alterado com sucesso.");
+    }
+    public void apagarLivro(String ID) {
+        if (livroDAO.delete(ID) == null) throw new LivroException("Nenhum livro foi com o ID utilizado (" + ID + ") foi encontrado no banco de dados.");
+        else {
+            System.out.println("Livro apagado.");
+            discoDAO.delete(ID);
+        }
+    }
+}
