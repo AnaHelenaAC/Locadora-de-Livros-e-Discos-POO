7cebaa275eac7e2a46f64813ec146a192c68fc25
Adriam Sousa <ratogordo27@gmail.com>
2026-06-01 03:42:32 -0300

    Uptade diretorios
    renomeei alguns diretorios errados.

--- src/main/java/br/edu/ufersa/locadora/model/DAO/DiscoDAO.java
+++ src/main/java/br/edu/ufersa/locadora/model/DAO/DiscoDAO.java
@@ -1,6 +1,6 @@
 package br.edu.ufersa.locadora.model.DAO;
 
-import src.main.java.br.edu.ufersa.locadora.model.entities.Disco;
+import br.edu.ufersa.locadora.model.entities.Disco;
 
 import java.util.List;
 import java.util.ArrayList;
@@ -8,9 +8,9 @@ import java.util.ArrayList;
 public class DiscoDAO {
     public void create(Disco disco) {
         String query = "INSERT INTO Discos (titulo, criadoPor, genero, valor, dataDeLancamento, qtdItens, duracao) VALUES (?, ?, ?, ?, ?, ?, ?)";
-        
-        try (Connection con = ConnectionFactory.getConnection(URL,USER,PASS); 
-             PreparedStatement ps = con.prepareStatement(query)) {
+
+        try (Connection con = ConnectionFactory.getConnection(URL, USER, PASS);
+                PreparedStatement ps = con.prepareStatement(query)) {
             ps.setString(1, disco.getTitulo());
             ps.setString(2, disco.getCriadoPor());
             ps.setString(3, disco.getGenero());
@@ -19,61 +19,65 @@ public class DiscoDAO {
             ps.setInt(6, disco.getQtdItens());
             ps.setInt(7, disco.getDuracao());
             ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-        catch (SQLException e) {e.printStackTrace();}
     }
+
     public List<Disco> read(String ID) {
         String query = "SELECT * FROM Discos WHERE ID = ?";
         List<Disco> discos = new ArrayList<>();
-        
-        try (Connection con = ConnectionFactory.getConnection(URL,USER,PASS); 
-             PreparedStatement ps = con.prepareStatement(query);
-             ResultSet rs = ps.executeQuery()) {
+
+        try (Connection con = ConnectionFactory.getConnection(URL, USER, PASS);
+                PreparedStatement ps = con.prepareStatement(query);
+                ResultSet rs = ps.executeQuery()) {
             while (rs.next()) {
-                    Disco disco = new Disco(
+                Disco disco = new Disco(
                         rs.getString("titulo"),
                         rs.getString("criadoPor"),
                         rs.getString("genero"),
                         rs.getDouble("valor"),
                         rs.getDate("dataDeLancamento"),
                         rs.getInt("qtdItens"),
-                        rs.getInt("duracao")
-                        );
-                    discos.add(disco);
+                        rs.getInt("duracao"));
+                discos.add(disco);
             }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-        catch (SQLException e) {e.printStackTrace();}
         return discos;
     }
+
     public Disco readByID(String ID) {
         String query = "SELECT * FROM Discos WHERE ID = ?";
         Disco disco = null;
-        
-        try (Connection con = ConnectionFactory.getConnection(URL,USER,PASS); 
-             PreparedStatement ps = con.prepareStatement(query)) {
+
+        try (Connection con = ConnectionFactory.getConnection(URL, USER, PASS);
+                PreparedStatement ps = con.prepareStatement(query)) {
             ps.setString(1, ID);
             try (ResultSet rs = ps.executeQuery()) {
                 if (rs.next()) {
                     disco = new Disco(
-                        rs.getString("titulo"),
-                        rs.getString("criadoPor"),
-                        rs.getString("genero"),
-                        rs.getDouble("valor"),
-                        rs.getDate("dataDeLancamento"),
-                        rs.getInt("qtdItens"),
-                        rs.getInt("duracao")
-                        );
+                            rs.getString("titulo"),
+                            rs.getString("criadoPor"),
+                            rs.getString("genero"),
+                            rs.getDouble("valor"),
+                            rs.getDate("dataDeLancamento"),
+                            rs.getInt("qtdItens"),
+                            rs.getInt("duracao"));
                 }
             }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-        catch (SQLException e) {e.printStackTrace();}
         return disco;
     }
+
     public void update(Disco disco) {
         String query = "UPDATE Discos SET titulo = ?, criadoPor = ?, genero = ?, valor = ?, dataDeLancamento = ?, qtdItens = ?, duracao = ? WHERE ID = ?";
-        
-        try (Connection con = ConnectionFactory.getConnection(URL,USER,PASS); 
-             PreparedStatement ps = con.prepareStatement(query)) {
+
+        try (Connection con = ConnectionFactory.getConnection(URL, USER, PASS);
+                PreparedStatement ps = con.prepareStatement(query)) {
             ps.setString(1, disco.getTitulo());
             ps.setString(2, disco.getCriadoPor());
             ps.setString(3, disco.getGenero());
@@ -83,17 +87,20 @@ public class DiscoDAO {
             ps.setInt(7, disco.getDuracao());
             ps.setString(8, disco.getID());
             ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-        catch (SQLException e) {e.printStackTrace();}
     }
+
     public void delete(String ID) {
         String query = "DELETE FROM Discos WHERE ID = ?";
-        
-        try (Connection con = ConnectionFactory.getConnection(URL,USER,PASS); 
-             PreparedStatement ps = con.prepareStatement(query)) {
+
+        try (Connection con = ConnectionFactory.getConnection(URL, USER, PASS);
+                PreparedStatement ps = con.prepareStatement(query)) {
             ps.setString(1, ID);
             ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-        catch (SQLException e) {e.printStackTrace();}
     }
 }
