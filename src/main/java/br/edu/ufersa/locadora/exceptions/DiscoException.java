c05f630f55e0184b369a7b0cd0f02d946197dece
AnaHelenaAC <ana.cosme@alunos.ufersa.edu.br>
2026-06-17 09:51:24 -0300

    Update DiscoException.java
    Alterando erros que foram encontrados posteriormente.

--- src/main/java/br/edu/ufersa/locadora/exceptions/DiscoException.java
+++ src/main/java/br/edu/ufersa/locadora/exceptions/DiscoException.java
@@ -1,7 +1,7 @@
 package br.edu.ufersa.locadora.exceptions;
 
-public class DiscoException extends Exception {
-    public DiscoException(String msg) {
-        super(msg);
+public class DiscoException extends RuntimeException {
+    public DiscoException(String mensagem) {
+        super(mensagem);
     }
 }
