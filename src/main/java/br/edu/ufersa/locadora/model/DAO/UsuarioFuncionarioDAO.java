31d2d6ab497653d790689b137c60539d0f70db12
AnaHelenaAC <ana.cosme@alunos.ufersa.edu.br>
2026-06-20 12:44:34 -0300

    Update UsuarioFuncionarioDAO.java

--- src/main/java/br/edu/ufersa/locadora/model/DAO/UsuarioFuncionarioDAO.java
+++ src/main/java/br/edu/ufersa/locadora/model/DAO/UsuarioFuncionarioDAO.java
@@ -1,125 +1,101 @@
 package br.edu.ufersa.locadora.model.DAO;
 
 import br.edu.ufersa.locadora.model.entities.UsuarioFuncionario;
+import br.edu.ufersa.locadora.exceptions.SemNomeException;
+import br.edu.ufersa.locadora.exceptions.UsuarioException;
 import br.edu.ufersa.locadora.exceptions.UsuarioFuncionarioException;
-import java.sql.*;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
 public class UsuarioFuncionarioDAO {
-    private final static String URL = "jdbc:mysql://localhost/poo";
-    private final static String USER = "poo";
-    private final static String PASS = "AH443162ah";
-    private static Connection con = null;
-
-    public static Connection getConnection(){
-        if(con == null){
-            try{
-                con = DriverManager.getConnection(URL,USER,PASS);
-            }catch (SQLException e){e.printStackTrace();}
-        }
-        return con;
-    }
-
     public UsuarioFuncionario Create(UsuarioFuncionario entity) throws UsuarioFuncionarioException {
-        con = getConnection();
-        String sql = "INSERT INTO tb_usu (nome, login, senha, is_gerente) VALUES (?, ?, ?, ?)";
+        String sql = "INSERT INTO Usuarios (nome, login, senha, isGerente) VALUES (?, ?, ?, ?)";
 
-        try {
-            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+        try (Connection con = ConnectionFactory.getConnection();
+             PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
             ps.setString(1, entity.getNome());
             ps.setString(2, entity.getLogin());
             ps.setString(3, entity.getSenha());
             ps.setBoolean(4, entity.isGerente());
-            ps.execute();
+            ps.executeUpdate();
 
-            ResultSet rs = ps.getGeneratedKeys();
-            if (rs != null && rs.next()) {
-                entity.setIdFuncionario(rs.getLong(1));
-                rs.close();
+            try (ResultSet rs = ps.getGeneratedKeys()) {
+                if (rs.next()) {
+                    entity.setIdFuncionario(rs.getLong(1));
+                }
             }
-            ps.close();
         } catch (Exception e) {
-            throw new UsuarioFuncionarioException("Erro ao inserir o funcionário: " + e.getMessage());
+            throw new RuntimeException("Erro ao inserir o funcionário: " + e.getMessage(), e);
         }
 
         return entity;
     }
 
     public List<UsuarioFuncionario> Read(String param) throws UsuarioFuncionarioException {
-        con = getConnection();
-        String sql = "SELECT * FROM tb_usu WHERE nome LIKE ? AND is_gerente = false";
+        String sql = "SELECT ID, nome, login, senha, isGerente FROM Usuarios WHERE nome LIKE ? AND isGerente = false";
         List<UsuarioFuncionario> lista = new ArrayList<>();
 
-        try{
-            PreparedStatement ps = con.prepareStatement(sql);
+        try (Connection con = ConnectionFactory.getConnection();
+             PreparedStatement ps = con.prepareStatement(sql)) {
             ps.setString(1, "%" + param + "%");
-            ResultSet rs = ps.executeQuery();
-
-            while (rs.next()) {
-                UsuarioFuncionario f = new UsuarioFuncionario();
-                f.setIdFuncionario(rs.getLong("id"));
-                f.setGerente(rs.getBoolean("is_gerente"));
-
-                try {
-                    f.setNome(rs.getString("nome"));
-                    f.setLogin(rs.getString("login"));
-                    f.setSenha(rs.getString("senha"));
-                } catch (Exception e) { e.printStackTrace(); }
-
-
-                lista.add(f);
+            try (ResultSet rs = ps.executeQuery()) {
+                while (rs.next()) {
+                    lista.add(mapearFuncionario(rs));
+                }
             }
-
-            rs.close();
-            ps.close();
         } catch (SQLException e){
-            throw new UsuarioFuncionarioException("Erro ao buscar funcionários: " + e.getMessage());
+            throw new RuntimeException("Erro ao buscar funcionários: " + e.getMessage(), e);
         }
         return lista;
     }
 
     public boolean Update(UsuarioFuncionario entity) throws UsuarioFuncionarioException {
-        con = getConnection();
-        String sql = "UPDATE tb_usu SET nome = ?, login = ?, senha = ?, is_gerente = ? WHERE id = ?";
-        boolean sucesso = false;
+        String sql = "UPDATE Usuarios SET nome = ?, login = ?, senha = ?, isGerente = ? WHERE ID = ?";
 
-        try {
-            PreparedStatement ps = con.prepareStatement(sql);
+        try (Connection con = ConnectionFactory.getConnection();
+             PreparedStatement ps = con.prepareStatement(sql)) {
             ps.setString(1, entity.getNome());
             ps.setString(2, entity.getLogin());
             ps.setString(3, entity.getSenha());
             ps.setBoolean(4, entity.isGerente());
             ps.setLong(5, entity.getIdFuncionario());
 
-            int linhasAfetadas = ps.executeUpdate();
-            if (linhasAfetadas > 0) {
-                sucesso = true;
-            }
-            ps.close();
+            return ps.executeUpdate() > 0;
         } catch (SQLException e) {
-            throw new UsuarioFuncionarioException("Erro ao atualizar o funcionário: " + e.getMessage());
+            throw new RuntimeException("Erro ao atualizar o funcionário: " + e.getMessage(), e);
         }
-        return sucesso;
     }
 
     public boolean Delete(UsuarioFuncionario entity) throws UsuarioFuncionarioException {
-        con = getConnection();
-        String sql = "DELETE FROM tb_usu WHERE id = ?";
-        boolean sucesso = false;
+        String sql = "DELETE FROM Usuarios WHERE ID = ?";
 
-        try {
-            PreparedStatement ps = con.prepareStatement(sql);
+        try (Connection con = ConnectionFactory.getConnection();
+             PreparedStatement ps = con.prepareStatement(sql)) {
             ps.setLong(1, entity.getIdFuncionario());
 
-            int linhasAfetadas = ps.executeUpdate();
-            if (linhasAfetadas > 0) {
-                sucesso = true;
-            }
-            ps.close();
+            return ps.executeUpdate() > 0;
         } catch (SQLException e) {
-            throw new UsuarioFuncionarioException("Erro ao deletar o funcionário: " + e.getMessage());
+            throw new RuntimeException("Erro ao deletar o funcionário: " + e.getMessage(), e);
         }
-        return sucesso;
+    }
+
+    private UsuarioFuncionario mapearFuncionario(ResultSet rs) throws SQLException {
+        try {
+            UsuarioFuncionario funcionario = new UsuarioFuncionario();
+            funcionario.setIdFuncionario(rs.getLong("ID"));
+            funcionario.setGerente(rs.getBoolean("isGerente"));
+            funcionario.setNome(rs.getString("nome"));
+            funcionario.setLogin(rs.getString("login"));
+            funcionario.setSenha(rs.getString("senha"));
+            return funcionario;
+        } catch (SemNomeException | UsuarioException | UsuarioFuncionarioException e) {
+            throw new RuntimeException("Erro ao mapear funcionário do banco: " + e.getMessage(), e);
+        }
     }
 }
