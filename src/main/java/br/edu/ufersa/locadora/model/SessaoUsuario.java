1912575c55a8a65dc4465274570c1cd10246ceec
AnaHelenaAC <ana.cosme@alunos.ufersa.edu.br>
2026-06-20 12:45:26 -0300

    Create SessaoUsuario.java

---
+++ src/main/java/br/edu/ufersa/locadora/model/SessaoUsuario.java
@@ -0,0 +1,102 @@
+package br.edu.ufersa.locadora.model;
+
+import br.edu.ufersa.locadora.model.Service.AluguelService;
+import br.edu.ufersa.locadora.model.Service.ClienteService;
+import br.edu.ufersa.locadora.model.Service.DiscoService;
+import br.edu.ufersa.locadora.model.Service.LivroService;
+import br.edu.ufersa.locadora.model.Service.RegistroService;
+import br.edu.ufersa.locadora.model.Service.UsuarioFuncionarioService;
+import br.edu.ufersa.locadora.model.Service.UsuarioGerenteService;
+import br.edu.ufersa.locadora.model.Service.UsuarioService;
+import br.edu.ufersa.locadora.model.entities.Usuario;
+
+public final class SessaoUsuario {
+
+    private static final SessaoUsuario INSTANCE = new SessaoUsuario();
+
+    private Usuario usuarioLogado;
+
+    private final UsuarioService usuarioService;
+    private final UsuarioGerenteService usuarioGerenteService;
+    private final UsuarioFuncionarioService usuarioFuncionarioService;
+    private final ClienteService clienteService;
+    private final LivroService livroService;
+    private final DiscoService discoService;
+    private final AluguelService aluguelService;
+    private final RegistroService registroService;
+
+    private SessaoUsuario() {
+        this.usuarioService = new UsuarioService();
+        this.usuarioGerenteService = new UsuarioGerenteService();
+        this.usuarioFuncionarioService = new UsuarioFuncionarioService();
+        this.clienteService = new ClienteService();
+        this.livroService = new LivroService(new br.edu.ufersa.locadora.model.DAO.LivroDAO());
+        this.discoService = new DiscoService(new br.edu.ufersa.locadora.model.DAO.DiscoDAO());
+        this.aluguelService = new AluguelService();
+        this.registroService = new RegistroService();
+    }
+
+    public static SessaoUsuario getInstance() {
+        return INSTANCE;
+    }
+
+    public Usuario getUsuario() {
+        return usuarioLogado;
+    }
+
+    public Usuario getUsuarioLogado() {
+        return usuarioLogado;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuarioLogado = usuario;
+    }
+
+    public void setUsuarioLogado(Usuario usuario) {
+        this.usuarioLogado = usuario;
+    }
+
+    public void limparSessao() {
+        this.usuarioLogado = null;
+    }
+
+    public boolean possuiUsuarioLogado() {
+        return usuarioLogado != null;
+    }
+
+    public boolean usuarioEhGerente() {
+        return usuarioLogado != null && usuarioLogado.isGerente();
+    }
+
+    public UsuarioService getUsuarioService() {
+        return usuarioService;
+    }
+
+    public UsuarioGerenteService getUsuarioGerenteService() {
+        return usuarioGerenteService;
+    }
+
+    public UsuarioFuncionarioService getUsuarioFuncionarioService() {
+        return usuarioFuncionarioService;
+    }
+
+    public ClienteService getClienteService() {
+        return clienteService;
+    }
+
+    public LivroService getLivroService() {
+        return livroService;
+    }
+
+    public DiscoService getDiscoService() {
+        return discoService;
+    }
+
+    public AluguelService getAluguelService() {
+        return aluguelService;
+    }
+
+    public RegistroService getRegistroService() {
+        return registroService;
+    }
+}
