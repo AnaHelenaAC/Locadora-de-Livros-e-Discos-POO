a36aab12f13467d9787b4cf4be40d9249e9db4c2
AnaHelenaAC <ana.cosme@alunos.ufersa.edu.br>
2026-06-23 08:42:28 -0300

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,11 +2,10 @@ module Locadora.Livros.e.Discos {
 
     requires javafx.controls;
     requires javafx.fxml;
+    requires transitive javafx.graphics;
     requires java.sql;
 
     exports br.edu.ufersa.locadora;
-
     opens br.edu.ufersa.locadora.controllers to javafx.fxml;
-
     opens br.edu.ufersa.locadora.model.entities to javafx.base;
 }
